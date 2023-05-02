f750258f399f4bb6cbdd4d248a765bbd9cbff265
brianach <>
2023-05-02 12:34:03 +0000

    add intital test and code

--- scripts/game.test.js
+++ scripts/game.test.js
@@ -1,3 +1,22 @@
+//const { default: test } = require('node:test');
+//const { describe } = require('yargs');
+const { game } = require('../game');
+
 /**
  * @jest-environment jsdom
  */
+
+beforeAll(() => {
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('index.html', 'utf-8');
+    document.open();
+    document.write(fileContents);
+    document.close;
+});
+
+describe('game object contains correct keys', () => {
+    test('score key exists', () => {
+        expect('score' in game).toBe(true);
+    });
+});
+
