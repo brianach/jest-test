1d6c35016c0811e08c77be145ad8963d2b319744
brianach <>
2023-05-02 12:49:14 +0000

    add all basic tests

--- scripts/game.test.js
+++ scripts/game.test.js
@@ -1,22 +1,32 @@
-//const { default: test } = require('node:test');
-//const { describe } = require('yargs');
-const { game } = require('../game');
-
 /**
  * @jest-environment jsdom
  */
 
+const { game } = require("../game");
+
+
 beforeAll(() => {
-    let fs = require('fs');
-    let fileContents = fs.readFileSync('index.html', 'utf-8');
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
     document.open();
     document.write(fileContents);
-    document.close;
+    document.close();
 });
 
-describe('game object contains correct keys', () => {
-    test('score key exists', () => {
-        expect('score' in game).toBe(true);
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
-
